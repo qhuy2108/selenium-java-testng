cc52365b4db4a68d93626ad33c5c0ad188f63aaf
qhuy2108 <qhuy2108@gmail.com>
2024-11-06 23:51:32 +0700

    new d6 m11

--- selenium/webdriver/Topic_00_Template.java
+++ selenium/webdriver/Topic_00_Template.java
@@ -16,11 +16,6 @@ public class Topic_00_Template {
     @BeforeClass
     public void beforeClass() {
 
-        if (osName.contains("Mac OS")) {
-            System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver.exe");
-        } else {
-            System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-        }
 
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
