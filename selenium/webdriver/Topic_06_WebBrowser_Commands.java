d76cbccb85f8df4acde3cb97ff4457900e3592aa
qhuy2108 <qhuy2108@gmail.com>
2024-11-07 01:41:15 +0700

    new d7 m11

---
+++ selenium/webdriver/Topic_06_WebBrowser_Commands.java
@@ -0,0 +1,45 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Topic_06_WebBrowser_Commands {
+    WebDriver driver;
+    WebElement element;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("");
+    }
+
+    @Test
+    public void TC_01_Run_On_Firefox() {
+        driver = new FirefoxDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+
+    @Test
+    public void TC_02_Run_On_Chrome() {
+        driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+
+    @Test
+    public void TC_03_Run_On_Edge() {
+        driver = new EdgeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.quit();
+    }
+}
